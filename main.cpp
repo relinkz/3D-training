4ac4fbb78355a2b0a64a5d1b9b58c7bd2b448dfd
relinkz <seb.lundgren@gmail.com>
2016-07-30 15:03:40 +0200

    added a basic window

---
+++ main.cpp
@@ -0,0 +1,101 @@
+//--------------------------------------------------------------------------------------
+// BTH - Stefan Petersson 2014. All rights reserved.
+// Added some comments (Francisco)
+//--------------------------------------------------------------------------------------
+#include <windows.h>
+
+HWND InitWindow(HINSTANCE hInstance);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+/*
+ Entry point for our program
+ This part of the code uses extensively Windows Datatypes, please the following
+ link for more detail about them,
+ https://msdn.microsoft.com/en-us/library/windows/desktop/aa383751(v=vs.85).aspx
+*/
+int WINAPI wWinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow )
+{
+	MSG msg = { 0 };
+
+	// create window
+	HWND wndHandle = InitWindow(hInstance);
+	
+	// window is valid
+	if (wndHandle)
+	{
+		// display window
+		ShowWindow(wndHandle, nCmdShow);
+		// enter message loop, loop until the message WM_QUIT is received.
+		while (WM_QUIT != msg.message)
+		{
+			// read messages
+			if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+			{
+				TranslateMessage(&msg);
+				// this will call the function WndProc below!
+				DispatchMessage(&msg);
+			}
+			else
+			{
+				// update/render goes here
+			}
+		}
+
+		// finish the program
+		DestroyWindow(wndHandle);
+	}
+
+	// return how the program finished.
+	return (int) msg.wParam;
+}
+
+HWND InitWindow(HINSTANCE hInstance)
+{
+	// Every window created must belong to a CLASS, so
+	// we first create a new class.
+	// fill in the WNDCLASSEX structure
+	WNDCLASSEX wcex = { 0 };
+	wcex.cbSize = sizeof(WNDCLASSEX); 
+	wcex.style          = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc    = WndProc;			// Which function is called for events
+	wcex.hInstance      = hInstance;
+	wcex.lpszClassName  = L"BasicWindow";
+	// use the struct to register the new class.
+	// the name of the class is "BasicWindow"
+	if( !RegisterClassEx(&wcex) )
+		return false;
+
+	// define a struct for the window size
+	RECT rc = { 0, 0, 640, 480 };
+	// create the window to this size and with additional properties
+	// (border, menu, etc)
+	AdjustWindowRect( &rc, WS_OVERLAPPEDWINDOW, FALSE );
+	
+	HWND handle = CreateWindow(
+		L"BasicWindow",			// CLASS, if does not exists fails!
+		L"BTH BasicWindow",		// Window name (title)
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		rc.right - rc.left,		// width
+		rc.bottom - rc.top,		// height
+		nullptr,
+		nullptr,
+		hInstance,
+		nullptr);
+
+	return handle;
+}
+
+LRESULT CALLBACK WndProc( HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam )
+{
+	switch (message) 
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;		
+	}
+
+	// if we do not handle the message here, simply call the Default handler function
+	return DefWindowProc(hWnd, message, wParam, lParam);
+}
